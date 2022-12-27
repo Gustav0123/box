6d2c4f0cfd63b9acc60fcf91d1eda8f0eeb4d295
Gustav0123 <121497839+Gustav0123@users.noreply.github.com>
2022-12-27 10:20:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+//crie as variaveis dos seus personagens aqui
+var box;
+
+
+function preload(){
+  //carregue as imagens dos personagens aqui
+}
+
+function setup() {
+  createCanvas(800,400);
+  //crie os sprites aqui
+  box = createSprite(200,200,50,80);
+  box.shapeColor = "green";
+
+  
+
+}
+
+function draw() {
+  background(255,255,255); 
+  if(keyIsDown(RIGHT_ARROW)){
+box.position.x=box.position.x+5
+
+  }
+if(keyIsDown(LEFT_ARROW)){
+box.position.x=box.position.x-5
+}
+
+  drawSprites();
+}
